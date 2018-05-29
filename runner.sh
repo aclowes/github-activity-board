fc61bf9b56eca00120293202b11fd04b4f3aba3d
Alec Clowes <aclowes@gmail.com>
2018-05-28 18:26:15 -0700

    update runner to skip yarn install

--- runner.sh
+++ runner.sh
@@ -12,17 +12,21 @@ pip install -q --upgrade pip setuptools
 pip install -q requests google-cloud-storage
 
 # install nodejs and yarn
-apt-get update -q
-apt-get install -q -y apt-transport-https
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+if [ ! `which yarn` ]
+then
+  apt-get update -q
+  apt-get install -q -y apt-transport-https
 
-curl -sS https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
-echo "deb https://deb.nodesource.com/node_8.x jessie main" | tee /etc/apt/sources.list.d/nodesource.list
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
-apt-get update -q
-apt-get install -q -y nodejs yarn
+  curl -sS https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+  echo "deb https://deb.nodesource.com/node_8.x jessie main" | tee /etc/apt/sources.list.d/nodesource.list
+
+  apt-get update -q
+  apt-get install -q -y nodejs yarn
+fi
 
 # calculate activity
 echo $GITHUB_ORGANIZATION
